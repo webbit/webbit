045e068ff2afba8054a0c5a5901f9e65ececbff3
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-02-04 10:28:10 +0000

    Added EmbeddedResourceHandler, which allows static resources to be embedded inside a jar file. Common stuff has been pulled up into an AbstractResourceHandler. Also deleted dupe StaticDirectoryHttpHandler[Test]. The main purpose of this patch is to embed useful things like DNode straight into the Webbit release jar (or at least a special packaging of it).

--- src/main/java/webbit/handler/StaticFileHandler.java
+++ src/main/java/webbit/handler/StaticFileHandler.java
@@ -1,62 +1,23 @@
 package webbit.handler;
 
-import webbit.HttpHandler;
+import webbit.HttpControl;
 import webbit.HttpRequest;
 import webbit.HttpResponse;
-import webbit.HttpControl;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Executor;
 
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
-public class StaticFileHandler implements HttpHandler {
-
-    public static final Map<String, String> DEFAULT_MIME_TYPES;
-
-    static {
-        // This is not an exhaustive list, just the most common types. Call registerMimeType() to add more.
-        Map<String, String> mimeTypes = new HashMap<String, String>();
-        mimeTypes.put("txt", "text/plain");
-        mimeTypes.put("css", "text/css");
-        mimeTypes.put("csv", "text/csv");
-        mimeTypes.put("htm", "text/html");
-        mimeTypes.put("html", "text/html");
-        mimeTypes.put("xml", "text/xml");
-        mimeTypes.put("js", "text/javascript"); // Technically it should be application/javascript (RFC 4329), but IE8 struggles with that
-        mimeTypes.put("xhtml", "application/xhtml+xml");
-        mimeTypes.put("json", "application/json");
-        mimeTypes.put("pdf", "application/pdf");
-        mimeTypes.put("zip", "application/zip");
-        mimeTypes.put("tar", "application/x-tar");
-        mimeTypes.put("gif", "image/gif");
-        mimeTypes.put("jpeg", "image/jpeg");
-        mimeTypes.put("jpg", "image/jpeg");
-        mimeTypes.put("tiff", "image/tiff");
-        mimeTypes.put("tif", "image/tiff");
-        mimeTypes.put("png", "image/png");
-        mimeTypes.put("svg", "image/svg+xml");
-        mimeTypes.put("ico", "image/vnd.microsoft.icon");
-        DEFAULT_MIME_TYPES = Collections.unmodifiableMap(mimeTypes);
-    }
-
-    private static final String DEFAULT_WELCOME_FILE = "index.html";
+public class StaticFileHandler extends AbstractResourceHandler {
 
     private final File dir;
-    private final Executor ioThread;
-    private final Map<String, String> mimeTypes;
-    private String welcomeFile;
 
     public StaticFileHandler(File dir, Executor ioThread) {
+        super(ioThread);
         this.dir = dir;
-        this.ioThread = ioThread;
-        this.mimeTypes = new HashMap<String, String>(DEFAULT_MIME_TYPES);
-        this.welcomeFile = DEFAULT_WELCOME_FILE;
     }
 
     public StaticFileHandler(String dir, Executor ioThread) {
@@ -71,149 +32,49 @@ public class StaticFileHandler implements HttpHandler {
         this(new File(dir));
     }
 
-    public StaticFileHandler addMimeType(String extension, String mimeType) {
-        mimeTypes.put(extension, mimeType);
-        return this;
+    protected StaticFileHandler.IOWorker createIOWorker(HttpRequest request, HttpResponse response, HttpControl control) {
+        return new StaticFileHandler.FileWorker(request.uri(), response, control);
     }
+    
+    protected class FileWorker extends IOWorker {
+        private File file;
 
-    public StaticFileHandler welcomeFile(String welcomeFile) {
-        this.welcomeFile = welcomeFile;
-        return this;
-    }
-
-    @Override
-    public void handleHttpRequest(final HttpRequest request, final HttpResponse response, final HttpControl control) throws Exception {
-        // Switch from web thead to IO thread, so we don't block web server when we access the filesystem.
-        ioThread.execute(new IOWorker(dir, request.uri(), welcomeFile) {
-            @Override
-            protected void notFound() {
-                // Switch back from IO thread to web thread.
-                control.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        control.nextHandler();
-                    }
-                });
-            }
-
-            @Override
-            protected void serve(final String filename, final byte[] contents) {
-                // Switch back from IO thread to web thread.
-                control.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        // TODO: Don't read all into memory, instead use zero-copy.
-                        // TODO: Check bytes read match expected encoding of mime-type
-                        response.header("Content-Type", guessMimeType(filename, response))
-                                .header("Content-Length", contents.length)
-                                .content(contents)
-                                .end();
-                    }
-                });
-            }
-
-            @Override
-            protected void error(final IOException exception) {
-                // Switch back from IO thread to web thread.
-                control.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        response.error(exception);
-                    }
-                });
-            }
-        });
-    }
-
-    private String guessMimeType(String path, HttpResponse response) {
-        int lastDot = path.lastIndexOf('.');
-        if (lastDot == -1) {
-            return null;
-        }
-        String extension = path.substring(lastDot + 1).toLowerCase();
-        String mimeType = mimeTypes.get(extension);
-        if (mimeType == null) {
-            return null;
-        }
-        if (mimeType.startsWith("text/") && response.charset() != null) {
-            mimeType += "; charset=" + response.charset().name();
-        }
-        return mimeType;
-    }
-
-    /**
-     * All IO is performed by this worker on a separate thread, so we never block the HttpHandler.
-     */
-    private abstract static class IOWorker implements Runnable {
-
-        private final File root;
-        private final String path;
-        private final String welcomeFile;
-
-        private IOWorker(File root, String path, String welcomeFile) {
-            this.root = root;
-            this.path = path;
-            this.welcomeFile = welcomeFile;
+        private FileWorker(String path, HttpResponse response, HttpControl control) {
+            super(path, response, control);
         }
 
         @Override
-        public void run() {
-            // TODO: Cache
-            // TODO: If serving directory and trailing slash omitted, perform redirect
-            try {
-                File file = resolveFile(path);
-                if (file == null || !file.exists()) {
-                    notFound();
-                } else if (!file.isDirectory()) {
-                    serve(file.getName(), read(file));
-                } else {
-                    file = new File(file, welcomeFile);
-                    if (file.exists()) {
-                        serve(file.getName(), read(file));
-                    } else {
-                        notFound();
-                    }
-                }
-            } catch (IOException e) {
-                error(e);
-            }
+        protected boolean exists() throws IOException {
+            file = resolveFile(path);
+            return file != null && file.exists();
+        }
+
+        @Override
+        protected byte[] fileBytes() throws IOException {
+            return file.isFile() ? read(file) : null;
+        }
+
+        @Override
+        protected byte[] welcomeBytes() throws IOException {
+            File welcome = new File(file, welcomeFileName);
+            return welcome.isFile() ? read(welcome) : null;
         }
 
         private byte[] read(File file) throws IOException {
-            byte[] data = new byte[(int) file.length()];
-            FileInputStream in = new FileInputStream(file);
-            try {
-                in.read(data);
-            } finally {
-                in.close();
-            }
-            return data;
+            return read((int) file.length(), new FileInputStream(file));
         }
 
         private File resolveFile(String path) throws IOException {
-
-            // Strip of query params
-            int queryStart = path.indexOf('?');
-            if (queryStart > -1) {
-                path = path.substring(0, queryStart);
-            }
-
             // Find file, relative to roo
-            File result = new File(root, path).getCanonicalFile();
+            File result = new File(dir, path).getCanonicalFile();
 
             // For security, check file really does exist under root.
             String fullPath = result.getPath();
-            if (!fullPath.startsWith(root.getCanonicalPath() + File.separator) && !fullPath.equals(root.getCanonicalPath())) {
+            if (!fullPath.startsWith(dir.getCanonicalPath() + File.separator) && !fullPath.equals(dir.getCanonicalPath())) {
                 // Prevent paths like http://foo/../../etc/passwd
                 return null;
             }
             return result;
         }
-
-        protected abstract void notFound();
-
-        protected abstract void serve(String filename, byte[] contents);
-
-        protected abstract void error(IOException exception);
     }
 }
