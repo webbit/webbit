6c2e0076931380b55746e965ee7c29db6495df32
Kushal Pisavadia <kushi.p@gmail.com>
2011-02-24 22:20:50 +0000

    cleaning up imports

--- src/main/java/org/webbitserver/stub/StubHttpRequest.java
+++ src/main/java/org/webbitserver/stub/StubHttpRequest.java
@@ -10,7 +10,6 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Implementation of HttpRequest that is easy to construct manually and populate.
