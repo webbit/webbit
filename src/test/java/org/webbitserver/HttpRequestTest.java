378e368ded3c139950b6b0743cc0395098e9de5d
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-16 16:19:01 +0000

    Return empty list instead of null

--- src/test/java/org/webbitserver/HttpRequestTest.java
+++ src/test/java/org/webbitserver/HttpRequestTest.java
@@ -3,10 +3,16 @@ package org.webbitserver;
 import org.junit.Test;
 import org.webbitserver.stub.StubHttpRequest;
 
+import java.util.Collections;
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class HttpRequestTest {
+    private static final List<String> EMPTY = Collections.emptyList();
+
     @Test
     public void extractsSingleQueryParameter() throws Exception {
         HttpRequest req = new StubHttpRequest("http://host.com:8080/path?fish=cod&fruit=orange");
@@ -17,5 +23,19 @@ public class HttpRequestTest {
     public void extractsMultipleQueryParameters() throws Exception {
         HttpRequest req = new StubHttpRequest("http://host.com:8080/path?fish=cod&fruit=orange&fish=smørflyndre");
         assertEquals(asList("cod", "smørflyndre"), req.queryParams("fish"));
+    }
+
+    @Test
+    public void alwaysReturnsEmptyListWhenThereIsNoQueryString() throws Exception {
+        HttpRequest req = new StubHttpRequest("http://host.com:8080/path");
+        assertEquals(EMPTY, req.queryParams("fish"));
+        assertNull(req.queryParam("fish"));
+    }
+
+    @Test
+    public void returnsEmptyLisrtWhenThereIsNoSuchParameter() throws Exception {
+        HttpRequest req = new StubHttpRequest("http://host.com:8080/path?poisson=cabillaud");
+        assertEquals(EMPTY, req.queryParams("fish"));
+        assertNull(req.queryParam("fish"));
     }
 }
