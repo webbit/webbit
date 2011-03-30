4a970f78c72818374e7be92cf6e050fdb34352d1
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-30 12:40:07 +0100

    Removed EventSource client, which has now moved to http://aslakhellesoy.github.com/eventsource-java. The Webbit EventSource server side support is integration-tested there (but webbit still has a HTML example that also serves as a manual integration test).

--- src/main/java/org/webbitserver/eventsource/MessageEmitter.java
+++
@@ -1,7 +0,0 @@
-package org.webbitserver.eventsource;
-
-interface MessageEmitter {
-    void emitMessage(String event, final MessageEvent message);
-
-    void setReconnectionTime(long reconnectionTimeMillis);
-}
--- src/main/java/org/webbitserver/eventsource/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains a Netty-based implementation of an <a href="http://dev.w3.org/html5/eventsource/">EventSource</a> client.
- * It is likely to be moved to a separate project, but it's currently kept in Webbit since it allows
- * end-to-end testing of Webbit's EventSource server support.
- */
-package org.webbitserver.eventsource;
