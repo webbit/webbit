977e730f9b7fa213f4b4a070b959c2b9dd24c84f
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-10-11 13:42:38 +0100

    All 9.* tests are passing - and it looks a little faster than jetty :-)

--- src/main/java/org/webbitserver/netty/HybiFrame.java
+++ src/main/java/org/webbitserver/netty/HybiFrame.java
@@ -1,31 +1,31 @@
 package org.webbitserver.netty;
 
 import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.buffer.DynamicChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.frame.TooLongFrameException;
 import org.webbitserver.WebSocketHandler;
 
-import java.nio.charset.Charset;
-
-import static org.jboss.netty.buffer.ChannelBuffers.wrappedBuffer;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HybiFrame {
 
-    public HybiFrame(int opcode, boolean fin, int rsv, ChannelBuffer data) {
+    private final int opcode;
+
+    private final boolean fin;
+    private final int rsv;
+    private List<ChannelBuffer> fragments = new ArrayList<ChannelBuffer>();
+
+    public HybiFrame(int opcode, boolean fin, int rsv, ChannelBuffer fragment) {
         this.opcode = opcode;
         this.fin = fin;
         this.rsv = rsv;
-        this.data = data;
+        fragments.add(fragment);
     }
 
-    private final int opcode;
-    private final boolean fin;
-    private final int rsv;
-    private ChannelBuffer data;
-
-    public void append(ChannelBuffer frame) {
-        // Creates CompositeChannelBuffer
-        data = wrappedBuffer(data, frame);
+    public void append(ChannelBuffer fragment) {
+        fragments.add(fragment);
     }
 
     public ChannelBuffer encode() throws TooLongFrameException {
@@ -37,47 +37,72 @@ public class HybiFrame {
         b0 |= opcode % 128;
 
         ChannelBuffer buffer;
-        int length = data.readableBytes();
+        int length = messageLength();
 
         if (opcode == Opcodes.OPCODE_PING && length > 125) {
             throw new TooLongFrameException("invalid payload for PING (payload length must be <= 125, was " + length);
         }
 
         if (length <= 125) {
-            buffer = createBuffer(data, 2);
+            buffer = createBuffer(length + 2);
             buffer.writeByte(b0);
             buffer.writeByte(length);
         } else if (length <= 0xFFFF) {
-            buffer = createBuffer(data, 4);
+            buffer = createBuffer(length + 4);
             buffer.writeByte(b0);
             buffer.writeByte(126);
             buffer.writeByte((length >>> 8) & 0xFF);
             buffer.writeByte((length) & 0xFF);
         } else {
-            buffer = createBuffer(data, 10);
+            buffer = createBuffer(length + 10);
             buffer.writeByte(b0);
             buffer.writeByte(127);
             buffer.writeLong(length);
         }
 
-        buffer.writeBytes(data, data.readerIndex(), data.readableBytes());
+        for (ChannelBuffer fragment : fragments) {
+            buffer.writeBytes(fragment, fragment.readerIndex(), fragment.readableBytes());
+        }
         return buffer;
     }
 
-    private ChannelBuffer createBuffer(ChannelBuffer data, int headerLength) {
-        return new DynamicChannelBuffer(data.order(), data.readableBytes() + headerLength);
+    private int messageLength() {
+        int n = 0;
+        for (ChannelBuffer fragment : fragments) {
+            n += fragment.readableBytes();
+        }
+        return n;
+    }
+
+    private byte[] messageBytes() {
+        byte[] result = new byte[messageLength()];
+        int offset = 0;
+        for (ChannelBuffer fragment : fragments) {
+            byte[] array = fragment.array();
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
+    }
+
+    private String messageString() throws UnsupportedEncodingException {
+        return new String(messageBytes(), "UTF-8");
+    }
+
+    private ChannelBuffer createBuffer(int length) {
+        return ChannelBuffers.buffer(length);
     }
 
     public void dispatch(WebSocketHandler handler, NettyWebSocketConnection connection) throws Throwable {
         switch (opcode) {
             case Opcodes.OPCODE_TEXT:
-                handler.onMessage(connection, data.toString(Charset.forName("UTF-8")));
+                handler.onMessage(connection, messageString());
                 return;
             case Opcodes.OPCODE_BINARY:
-                handler.onMessage(connection, data.array());
+                handler.onMessage(connection, messageBytes());
                 return;
             case Opcodes.OPCODE_PONG:
-                handler.onPong(connection, data.toString(Charset.forName("UTF-8")));
+                handler.onPong(connection, messageString());
                 return;
             default:
                 throw new IllegalStateException("Unexpected opcode:" + opcode);
