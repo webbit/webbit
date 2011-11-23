034ab42492ebe9232f6ea6ea5a1b04b421f9ac8e
Nathan Mische <nmische@gmail.com>
2011-11-22 21:41:27 -0500

    Updated to generate Flash socket policy file based on webserver's public URI.

--- src/main/java/org/webbitserver/netty/FlashPolicyFileDecoder.java
+++ src/main/java/org/webbitserver/netty/FlashPolicyFileDecoder.java
@@ -8,13 +8,36 @@ import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.handler.codec.frame.FrameDecoder;
 import org.jboss.netty.util.CharsetUtil;
 
+/**
+ * Checks the received {@link org.jboss.netty.buffer.ChannelBuffer
+ * ChannelBuffer}s for Flash policy file requests.
+ * 
+ * <p>
+ * If this decoder detects a Flash policy file request it adds a
+ * {@link FlashPolicyFileHandler} to the
+ * {@link org.jboss.netty.channel.ChannelPipeline ChannelPipeline} and removes
+ * itself from the pipeline. If a Flash policy file request is not detected in
+ * the first 23 bytes of the buffer, the decoder removes itself from the
+ * pipeline.
+ * <p>
+ * 
+ * <p>
+ * This implementation is based on the
+ * "replacing a decoder with another decoder in a pipeline" section of the
+ * {@link org.jboss.netty.handler.codec.frame.FrameDecoder FrameDecoder}
+ * documentation.
+ * </p>
+ */
 public class FlashPolicyFileDecoder extends FrameDecoder {
+
+	private final int publicPort;
 
 	private static final ChannelBuffer FLASH_POLICY_REQUEST = ChannelBuffers
 			.copiedBuffer("<policy-file-request/>\0", CharsetUtil.US_ASCII);
 
-	public FlashPolicyFileDecoder() {
+	public FlashPolicyFileDecoder(int publicPort) {
 		super(true);
+		this.publicPort = publicPort;
 	}
 
 	@Override
@@ -28,7 +51,7 @@ public class FlashPolicyFileDecoder extends FrameDecoder {
 
 			if (FLASH_POLICY_REQUEST.equals(firstMessage)) {
 				p.addAfter("flashpolicydecoder", "flashpolicyhandler",
-						new FlashPolicyFileHandler());
+						new FlashPolicyFileHandler(this.publicPort));
 			}
 
 			p.remove(this);
--- src/main/java/org/webbitserver/netty/FlashPolicyFileHandler.java
+++ src/main/java/org/webbitserver/netty/FlashPolicyFileHandler.java
@@ -1,7 +1,5 @@
 package org.webbitserver.netty;
 
-import java.net.InetSocketAddress;
-
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
@@ -13,29 +11,45 @@ import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.util.CharsetUtil;
 
+/**
+ * Responds with a Flash socket policy file.
+ * 
+ * <p>
+ * This implementation is based on the 
+ * <a href="https://github.com/waywardmonkeys/netty-flash-crossdomain-policy-server"
+ * >waywardmonkeys/netty-flash-crossdomain-policy-server</a> project and the 
+ * <a href="http://www.adobe.com/devnet/flashplayer/articles/socket_policy_files.html"
+ * ><em>Setting up a socket policy file server</em></a> article.
+ * </p>
+ */
+
 public class FlashPolicyFileHandler extends SimpleChannelUpstreamHandler {
+
+	private final int publicPort;
+
+	public FlashPolicyFileHandler(int publicPort) {
+		super();
+		this.publicPort = publicPort;
+	}
 
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
 			throws Exception {
 		Channel ch = e.getChannel();
-		ChannelBuffer response = getPolicyFileContents(ch);
+		ChannelBuffer response = getPolicyFileContents();
 		ChannelFuture future = ch.write(response);
 		future.addListener(ChannelFutureListener.CLOSE);
 		ctx.getPipeline().remove(this);
 	}
 
-	private ChannelBuffer getPolicyFileContents(Channel ch) throws Exception {
-
-		InetSocketAddress address = (InetSocketAddress) ch.getLocalAddress();
-		int port = address.getPort();
+	private ChannelBuffer getPolicyFileContents() throws Exception {
 
 		return ChannelBuffers.copiedBuffer(
 			"<?xml version=\"1.0\"?>\r\n"
 			+ "<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\r\n"
 			+ "<cross-domain-policy>\r\n"
 			+ "  <site-control permitted-cross-domain-policies=\"master-only\"/>\r\n"
-			+ "  <allow-access-from domain=\"*\" to-ports=\"" + port + "\" />\r\n"
+			+ "  <allow-access-from domain=\"*\" to-ports=\"" + this.publicPort + "\" />\r\n"
 			+ "</cross-domain-policy>\r\n",
 			CharsetUtil.US_ASCII);
 	}
