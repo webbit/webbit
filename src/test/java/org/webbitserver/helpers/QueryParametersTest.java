226d98bc4e7b51485fa107429f9d65aa01ebcbda
Kushal Pisavadia <kushi.p@gmail.com>
2011-11-26 12:16:07 +0000

    Make method naming consistent
    Convert from unix_style, to camelCase

--- src/test/java/org/webbitserver/helpers/QueryParametersTest.java
+++ src/test/java/org/webbitserver/helpers/QueryParametersTest.java
@@ -1,28 +1,28 @@
 package org.webbitserver.helpers;
 
-import org.junit.Test;
-
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class QueryParametersTest {
     @Test
-    public void parses_one_parameter() throws Exception {
+    public void parsesOneParameter() throws Exception {
         assertEquals("bar", new QueryParameters("foo=bar").first("foo"));
     }
 
     @Test
-    public void parses_two_parameters() throws Exception {
+    public void parsesTwoParameters() throws Exception {
         assertEquals(asList("b", "e"), new QueryParameters("a=b&c=d&a=e").all("a"));
     }
 
     @Test
-    public void parses_empty_parameter() throws Exception {
+    public void parsesEmptyParameter() throws Exception {
         assertEquals(null, new QueryParameters("a=").first("a"));
     }
 
     @Test
-    public void parses_mix_of_present_and_empty_parameters() throws Exception {
+    public void parsesMixOfPresentAndEmptyParameters() throws Exception {
         assertEquals(asList("b", null, "e"), new QueryParameters("a=b&a=&a=e").all("a"));
     }
 }
--- src/test/java/org/webbitserver/helpers/UTF8OutputTest.java
+++ src/test/java/org/webbitserver/helpers/UTF8OutputTest.java
@@ -10,24 +10,24 @@ public class UTF8OutputTest {
     private static UTF8Output utf8Output = new UTF8Output();
 
     @Test
-    public void decodes_latin() throws IOException {
+    public void decodesLatin() throws IOException {
         assertUtf8("Hellesøy");
     }
 
     @Test
-    public void decodes_chinese() throws IOException {
+    public void decodesChinese() throws IOException {
         assertUtf8("我希望有人告诉我数字相加的结果");
     }
 
     @Test
-    public void decodes_many_times() throws IOException {
+    public void decodesManyTimes() throws IOException {
         assertUtf8("Hellesøy 我希望有人告诉我数字相加的结果");
         assertUtf8("Hellesøy");
         assertUtf8("我希望有人告诉我数字相加的结果");
     }
 
     @Test(expected = UTF8Exception.class)
-    public void throws_on_non_utf_8_bytes() {
+    public void throwsOnNonUTF8Bytes() {
         int[] bad = new int[]{
                 0xCE, 0xBA, 0xE1, 0xBD, 0xB9, 0xCF, 0x83, 0xCE, 0xBC, 0xCE, 0xB5, 0xED, 0xA0, 0x80, 0x65, 0x64, 0x69, 0x74, 0x65, 0x64
         };
