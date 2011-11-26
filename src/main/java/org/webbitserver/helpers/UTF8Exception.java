2a60c6a4edc9fc16dfae464450899a5f7bab6250
Kushal Pisavadia <kushi.p@gmail.com>
2011-11-26 15:42:17 +0000

    Provide a serialVersionUID for Serializable classes
    This will make it easier to track differences in serialised classes when
    logs are being post-processed or compared over time.

--- src/main/java/org/webbitserver/helpers/UTF8Exception.java
+++ src/main/java/org/webbitserver/helpers/UTF8Exception.java
@@ -1,7 +1,9 @@
 package org.webbitserver.helpers;
 
 public class UTF8Exception extends RuntimeException {
-    public UTF8Exception(String reason) {
+	private static final long serialVersionUID = 3599440257713569059L;
+
+	public UTF8Exception(String reason) {
         super(reason);
     }
 }
