04fbc6f36d875a99f60ef1e9844c21c129cd5a5c
Kushal Pisavadia <kushi.p@gmail.com>
2011-11-27 03:05:15 +0000

    Add unit tests for EventSourceMessage
    - provide tests for cases where encoding wasn't covered such as
      comments, events, and retry
    - add tests to cover branching steps for both constructor and inner
      private methods
    - update HISTORY

--- src/test/java/org/webbitserver/netty/contrib/EventSourceMessageTest.java
+++ src/test/java/org/webbitserver/netty/contrib/EventSourceMessageTest.java
@@ -1,10 +1,15 @@
 package org.webbitserver.netty.contrib;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 public class EventSourceMessageTest {
+    @Test
+    public void skipsColonIfValueEmpty() throws Exception {
+        assertEquals("id\n", new EventSourceMessage().id("").build());
+    }
+
     @Test
     public void encodesSingleLine() throws Exception {
         assertEquals("data: Hello\n", new EventSourceMessage().data("Hello").build());
@@ -21,7 +26,49 @@ public class EventSourceMessageTest {
     }
 
     @Test
-    public void skipsColonIfValueEmpty() throws Exception {
-        assertEquals("id\n", new EventSourceMessage().id("").build());
+    public void encodesComment() throws Exception {
+        assertEquals("data: hello\n: hi there\n",
+            new EventSourceMessage().data("hello").comment("hi there").build());
+    }
+
+    @Test
+    public void encodesIdAsLong() throws Exception {
+        assertEquals("id: 40\n", new EventSourceMessage().id(40L).build());
+    }
+
+    @Test
+    public void encodesIdAsString() throws Exception {
+        assertEquals("id: somehash\n", new EventSourceMessage().id("somehash").build());
+    }
+
+    @Test
+    public void encodesEvent() throws Exception {
+        assertEquals("event: house party!\n",
+            new EventSourceMessage().event("house party!").build());
+    }
+
+    @Test
+    public void encodesRetry() throws Exception {
+        assertEquals("retry: 3\n", new EventSourceMessage().retry(3L).build());
+    }
+
+    @Test
+    public void emptyBuild() throws Exception {
+        assertEquals("", new EventSourceMessage().build());
+    }
+
+    @Test
+    public void buildsMessageWithData() throws Exception {
+        assertEquals("data: testing\n", new EventSourceMessage("testing").build());
+    }
+
+    @Test
+    public void buildsMessageWithEmptyData() throws Exception {
+        assertEquals("\n", new EventSourceMessage("").build());
+    }
+
+    @Test
+    public void buildsMessageWithNullId() throws Exception {
+        assertEquals("id\n", new EventSourceMessage().id(null).build());
     }
 }
