b697490fd9ceeac19d317080d5d6f2599e711378
DFUK <SELondon@drwuk.com>
2012-01-31 11:26:28 +0000

    Don't swallow exceptions

--- src/test/java/org/webbitserver/handler/SslTest.java
+++ src/test/java/org/webbitserver/handler/SslTest.java
@@ -1,24 +1,22 @@
 package org.webbitserver.handler;
 
-import java.io.IOException;
-import java.io.FileInputStream;
-import javax.net.ssl.HttpsURLConnection;
-
-import org.junit.Test;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.webbitserver.WebServer;
-import org.webbitserver.handler.ServerHeaderHandler;
-import org.webbitserver.handler.StringHttpHandler;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.FileInputStream;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 import static org.junit.Assert.assertEquals;
 import static org.webbitserver.WebServers.createWebServer;
 import static org.webbitserver.testutil.HttpClient.contents;
 import static org.webbitserver.testutil.HttpClient.httpsGet;
-
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import javax.net.ssl.SSLContext;
-import java.security.cert.X509Certificate;
 
 public class SslTest {
 
@@ -34,7 +32,7 @@ public class SslTest {
         webServer.start();
 
         try {
-            HttpsURLConnection  urlConnection = httpsGet(webServer, "/");
+            HttpsURLConnection urlConnection = httpsGet(webServer, "/");
             assertEquals("My Server", urlConnection.getHeaderField("Server"));
             assertEquals("body", contents(urlConnection));
         } finally {
@@ -43,22 +41,25 @@ public class SslTest {
     }
 
     @BeforeClass
-    public static void disableCertValidationSetUp() {
+    public static void disableCertValidationSetUp() throws NoSuchAlgorithmException, KeyManagementException {
         // Create a trust manager that does not validate certificate chains
         TrustManager[] trustAllCerts = new TrustManager[]{
-            new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() { return null; }
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-            }
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
         };
 
         // Install the all-trusting trust manager
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-        }
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 }
