a7d807824962d0f001971f3646125c930caacbcc
Daniel Wagner-Hall <dawagner@gmail.com>
2012-05-31 11:57:38 +0100

    Tidying up in response to review comments.
    Pulling out public, top-level classes, adding some javadoc.

--- src/main/java/org/webbitserver/handler/DirectoryListingFormatter.java
+++ src/main/java/org/webbitserver/handler/DirectoryListingFormatter.java
@@ -5,5 +5,13 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public interface DirectoryListingFormatter {
+  /**
+   * Formats a list of files for display as a full well-formed HTML page.
+   * Must escape any unsafe file names or other data being returned.
+   *
+   * @param files A list of files about which information (e.g. file names) is to be presented.
+   * @return A ByteBuffer containing an entire HTML page to display, presenting information about files.
+   * @throws IOException
+   */
   ByteBuffer formatFileListAsHtml(File[] files) throws IOException;
 }
