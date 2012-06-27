c14f2a567db4a9bbf980500865e01e8752a62ba6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-06-27 09:10:22 +0100

    Code formatting

--- src/main/java/org/webbitserver/helpers/NamedThreadFactory.java
+++ src/main/java/org/webbitserver/helpers/NamedThreadFactory.java
@@ -32,40 +32,40 @@ import java.util.concurrent.atomic.AtomicInteger;
  * {@link Executors#defaultThreadFactory()}.
  */
 public class NamedThreadFactory implements ThreadFactory {
-  private static final AtomicInteger threadPoolNumber = new AtomicInteger(1);
-  private final ThreadGroup group;
-  private final AtomicInteger threadNumber = new AtomicInteger(1);
-  private static final String NAME_PATTERN = "%s-%d-thread";
-  private final String threadNamePrefix;
+    private static final AtomicInteger threadPoolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private static final String NAME_PATTERN = "%s-%d-thread";
+    private final String threadNamePrefix;
 
-  /**
-   * Creates a new {@link NamedThreadFactory} instance
-   * 
-   * @param threadNamePrefix the name prefix assigned to each thread created.
-   */
-  public NamedThreadFactory(String threadNamePrefix) {
-    final SecurityManager s = System.getSecurityManager();
-    group = (s != null) ? s.getThreadGroup() : Thread.currentThread()
-        .getThreadGroup();
-    this.threadNamePrefix = String.format(NAME_PATTERN,
-        checkPrefix(threadNamePrefix), threadPoolNumber.getAndIncrement());
-  }
+    /**
+     * Creates a new {@link NamedThreadFactory} instance
+     *
+     * @param threadNamePrefix the name prefix assigned to each thread created.
+     */
+    public NamedThreadFactory(String threadNamePrefix) {
+        final SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread()
+                .getThreadGroup();
+        this.threadNamePrefix = String.format(NAME_PATTERN,
+                checkPrefix(threadNamePrefix), threadPoolNumber.getAndIncrement());
+    }
 
-  private static String checkPrefix(String prefix) {
-    return prefix == null || prefix.length() == 0 ? "Lucene" : prefix;
-  }
+    private static String checkPrefix(String prefix) {
+        return prefix == null || prefix.length() == 0 ? "Lucene" : prefix;
+    }
 
-  /**
-   * Creates a new {@link Thread}
-   * 
-   * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
-   */
-  public Thread newThread(Runnable r) {
-    final Thread t = new Thread(group, r, String.format("%s-%d",
-        this.threadNamePrefix, threadNumber.getAndIncrement()), 0);
-    t.setDaemon(true);
-    t.setPriority(Thread.NORM_PRIORITY);
-    return t;
-  }
+    /**
+     * Creates a new {@link Thread}
+     *
+     * @see java.util.concurrent.ThreadFactory#newThread(java.lang.Runnable)
+     */
+    public Thread newThread(Runnable r) {
+        final Thread t = new Thread(group, r, String.format("%s-%d",
+                this.threadNamePrefix, threadNumber.getAndIncrement()), 0);
+        t.setDaemon(true);
+        t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
 
 }
