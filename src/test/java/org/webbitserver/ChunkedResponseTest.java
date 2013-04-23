61aed306faad05401b6e235847c8ba12675f5e44
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2013-04-23 13:26:26 +0100

    Reformat

--- src/test/java/org/webbitserver/ChunkedResponseTest.java
+++ src/test/java/org/webbitserver/ChunkedResponseTest.java
@@ -2,15 +2,8 @@ package org.webbitserver;
 
 import org.junit.After;
 import org.junit.Test;
-import org.webbitserver.HttpControl;
-import org.webbitserver.HttpHandler;
-import org.webbitserver.HttpRequest;
-import org.webbitserver.HttpResponse;
-import org.webbitserver.WebServer;
-
 
 import java.net.URLConnection;
-
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertTrue;
@@ -25,36 +18,44 @@ public class ChunkedResponseTest {
         webServer.stop().get();
     }
 
-     @Test
+    @Test
     public void streamingViaChunks() throws Exception {
         webServer.add("/chunked", new HttpHandler() {
             @Override
             public void handleHttpRequest(HttpRequest req, final HttpResponse res, HttpControl control) {
-                control.execute(new Runnable() { 
+                control.execute(new Runnable() {
                     public void run() {
                         res.chunked();
-                        try{Thread.sleep(10);}catch (Exception ex){}
+                        nap();
                         res.write("chunk1");
-                        try{Thread.sleep(10);}catch (Exception ex){}
+                        nap();
                         res.write("chunk2");
-                        try{Thread.sleep(10);}catch (Exception ex){}
+                        nap();
                         res.end();
-                    }});
+                    }
+
+                    private void nap() {
+                        try {
+                            Thread.sleep(10);
+                        } catch (Exception ex) {
+                        }
+                    }
+                });
             }
         }).start().get();
-        
+
 
         URLConnection conn = httpGet(webServer, "/chunked");
 
         assertTrue("should contain chunks", stringify(conn.getInputStream()).equals("chunk1chunk2"));
         assertTrue("should contain Transfer-Encoding header", conn.getHeaderFields().get("Transfer-Encoding") != null);
-        assertTrue("should have chunked value in Transfer encoding header",conn.getHeaderFields().get("Transfer-Encoding").get(0).equals("chunked"));
+        assertTrue("should have chunked value in Transfer encoding header", conn.getHeaderFields().get("Transfer-Encoding").get(0).equals("chunked"));
     }
 
     private static String stringify(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
     }
-    
-   
+
+
 }
