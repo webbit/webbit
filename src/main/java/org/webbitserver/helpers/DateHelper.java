dbfda73e8656452e56930679e855fa61d1559116
peter royal <peter.royal@pobox.com>
2013-08-20 09:46:13 -0500

    RFC1123 means GMT dates

--- src/main/java/org/webbitserver/helpers/DateHelper.java
+++ src/main/java/org/webbitserver/helpers/DateHelper.java
@@ -3,12 +3,15 @@ package org.webbitserver.helpers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateHelper {
     private static final ThreadLocal<DateFormat> RFC_1123 = new ThreadLocal<DateFormat>() {
         @Override
         protected DateFormat initialValue() {
-            return new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+            SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+            format.setTimeZone(TimeZone.getTimeZone("GMT"));
+            return format;
         }
     };
 
