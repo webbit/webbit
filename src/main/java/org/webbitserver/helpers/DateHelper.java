db5a7ce8dcd4049ec4b5b3d2de51014a01693e44
Bugs Everywhere <lostdj@gmail.com>
2014-02-01 03:36:35 -0800

    DateHelper: locale fix.

--- src/main/java/org/webbitserver/helpers/DateHelper.java
+++ src/main/java/org/webbitserver/helpers/DateHelper.java
@@ -3,13 +3,14 @@ package org.webbitserver.helpers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class DateHelper {
     private static final ThreadLocal<DateFormat> RFC_1123 = new ThreadLocal<DateFormat>() {
         @Override
         protected DateFormat initialValue() {
-            SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+            SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
             format.setTimeZone(TimeZone.getTimeZone("GMT"));
             return format;
         }
